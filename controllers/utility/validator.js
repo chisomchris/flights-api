a9b72c63c0a00204f0b12652b1ed6c103ea03222
CHRISTIAN EZEANYAECHE <106704559+chisomchris@users.noreply.github.com>
2022-11-05 11:06:38 +0100

    Added date string validation

--- controllers/utility/validator.js
+++ controllers/utility/validator.js
@@ -5,3 +5,10 @@ exports.vString = (input) => {
 exports.vNumber = (input) => {
     return input && typeof input === 'number'
 }
+
+exports.vDateString = ( date ) => {
+    if (!(typeof date === 'string')) throw new Error("not a string")
+    // regex for DD/MM/YYYY or DD-MM-YYYY
+    const dateFormat =  /(((0[1-9]|[12][0-9]|3[01])([/-])(0[13578]|10|12)([/-])(\d{4}))|(([0][1-9]|[12][0-9]|30)([/-])(0[469]|11)([/-])(\d{4}))|((0[1-9]|1[0-9]|2[0-8])([/-])(02)([/-])(\d{4}))|((29)(\/)(02)([/-])([02468][048]00))|((29)([/-])(02)([/-])([13579][26]00))|((29)([/-])(02)([/-])([0-9][0-9][0][48]))|((29)([/-])(02)([/-])([0-9][0-9][2468][048]))|((29)([/-])(02)([/-])([0-9][0-9][13579][26])))/
+    return dateFormat.test(date)
+}
