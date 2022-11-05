fb0e7243746c82aef84f6f2e106c3edc51db09c7
CHRISTIAN EZEANYAECHE <106704559+chisomchris@users.noreply.github.com>
2022-11-05 12:14:45 +0100

    Changed date format

--- models/flights.js
+++ models/flights.js
@@ -3,70 +3,70 @@ exports.flights = [
         "title": "flight to algeria",
         "time": "1pm",
         "price": 39000,
-        "date": "2021-11-12",
+        "date": "12-12-2022",
         "id": "530eb310-0caa-46d8-b4d1-7d1ad4a9d852"
     },
     {
         "title": "flight to niger",
         "time": "5pm",
         "price": 23000,
-        "date": "2021-11-12",
+        "date": "10-10-2022",
         "id": "0b149ff1-cdb8-439c-bce7-65116637cb58"
     },
     {
         "title": "flight to ganacho",
         "time": "5am",
-        "price": 900,
-        "date": "2021-11-12",
+        "price": 45000,
+        "date": "01-01-2022",
         "id": "0c83676b-c498-4374-a32e-378b39eed2af"
     },
     {
         "title": "flight to germany",
         "time": "2pm",
         "price": 90000,
-        "date": "2021-11-12",
+        "date": "12-01-2023",
         "id": "ba711ef9-1d36-4297-acef-eeebc1d6c0ee"
     },
     {
         "title": "flight to senegal",
         "time": "2pm",
         "price": 50000,
-        "date": "2021-11-12",
+        "date": "10-12-2022",
         "id": "7b7340ab-ce8a-43b3-9556-b655d8389fc4"
     },
     {
         "title": "flight to egypt",
         "time": "2pm",
         "price": 19000,
-        "date": "2021-11-12",
+        "date": "21-12-2022",
         "id": "fa732358-bd0b-45f5-bfd3-a9e47e21c862"
     },
     {
         "title": "flight to armenia",
         "time": "2pm",
         "price": 20500,
-        "date": "2021-11-12",
+        "date": "15-11-2022",
         "id": "50b0ba6a-7598-4f79-9c21-432c37182b05"
     },
     {
         "title": "flight to england",
         "time": "4pm",
         "price": 90000,
-        "date": "2021-11-12",
+        "date": "11-11-2022",
         "id": "70c20eeb-8e2d-45d9-a4f5-81d197896713"
     },
     {
         "title": "flight to greece",
         "time": "2pm",
         "price": 55000,
-        "date": "2021-11-12",
+        "date": "25-12-2022",
         "id": "cffbe88d-cd54-48d3-ac11-d723d73996be"
     },
     {
         "title": "flight to italy",
         "time": "2pm",
         "price": 29000,
-        "date": "2021-11-12",
+        "date": "01-01-2023",
         "id": "1d33070b-1588-414e-91f5-c14530fe351b"
     },
 ]
